82a66e6d9a984dcf62a37aaad74a91ee4939941f
AaryaPakhale <127590975+AaryaPakhale@users.noreply.github.com>
2024-11-30 21:35:59 +0530

    Update tracker.py

--- object_tracker/tracker.py
+++ object_tracker/tracker.py
@@ -1,60 +1,3 @@
-# from ultralytics import YOLO
-# import cv2
-#
-# class Tracker:
-#
-#     def __init__(self):
-#         self.model = YOLO("yolov8x.pt")
-#
-#     def detect_objects(self, frames):
-#
-#         detections = []
-#
-#         for frame in frames:
-#             deteced_objs = self.detect_frame(frame)
-#             detections.append(deteced_objs)
-#         # for i in range(0, len(frames),5):
-#         #     frame = frames[i]
-#         #     detected_objs = self.detect_frame(frame)
-#         #     detections.append(detected_objs)
-#
-#         return detections
-#
-#     def detect_frame(self,frame):
-#         results = self.model.track(frame, persist=True)[0]
-#         name_dict = results.names
-#
-#         dict = {}
-#         for box in results.boxes:
-#             track_id = int(box.id.tolist()[0])
-#             result = box.xyxy.tolist()[0]
-#             object_class_id = box.cls.tolist()[0]
-#             object_class_name = name_dict[object_class_id]
-#             if object_class_name == "car":
-#                 dict[track_id] = result
-#
-#         return dict
-#
-#     def draw_annotations(self, frames, object_detections):
-#         output_video_frames = []
-#         for frame, obj_detected in zip(frames, object_detections):
-#             for track_id, bbox in obj_detected.items():
-#                 x1,y1,x2,y2 = bbox
-#
-#                 cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0,0,255), 2)
-#             output_video_frames.append(frame)
-#
-#         return output_video_frames
-#
-# # 1: 'bicycle',
-# # 2: 'car',
-# # 3: 'motorcycle',
-# # 5: 'bus',
-# # 7: 'truck'
-
-
-
-
 from ultralytics import YOLO
 import cv2
 import random
@@ -127,10 +70,3 @@ class Tracker:
             output_video_frames.append(frame)
 
         return output_video_frames
-
-# 1: 'bicycle',
-# 2: 'car',
-# 3: 'motorcycle',
-# 5: 'bus',
-# 7: 'truck'
-# 8: 'autorikshaw"
